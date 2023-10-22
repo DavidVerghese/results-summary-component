ddd2f4a1fd9aaf5fbff3327ce5e3971051f24efb
David Verghese <dj.verghese@gmail.com>
2023-10-22 16:30:59 -0400

    further work on shape of elements

--- src/App.js
+++ src/App.js
@@ -8,29 +8,29 @@ function App() {
 		{
 			"category": "Reaction",
 			"score": 80,
-			"backgroundColor" : "bg-red-300/25 p-1",
-			"textColor": "font-bold text-red-800/70 rounded-md",
+			"backgroundColor" : "bg-red-300/25",
+			"textColor": "font-bold text-red-800/70",
 			"icon": "assets/images/icon-reaction.svg"
 		},
 		{
 			"category": "Memory",
 			"score": 92,
-			"backgroundColor" : "bg-yellow-200/25 p-1",
-			"textColor": "font-bold text-yellow-600/80 rounded-md",
+			"backgroundColor" : "bg-yellow-200/25",
+			"textColor": "font-bold text-yellow-600/80",
 			"icon": "assets/images/icon-memory.svg"
 		},
 		{
 			"category": "Verbal",
 			"score": 61,
-			"backgroundColor" : "bg-green-200/25 p-1",
-			"textColor": "font-bold text-green-900/70 rounded-md",
+			"backgroundColor" : "bg-green-200/25",
+			"textColor": "font-bold text-green-900/70",
 			"icon": "assets/images/icon-verbal.svg"
 		},
 		{
 			"category": "Visual",
 			"score": 72,
 			"backgroundColor" : "bg-blue-200/25",
-			"textColor": "font-bold text-indigo-900/90 rounded-md",
+			"textColor": "font-bold text-indigo-900/90",
 			"icon": "assets/images/icon-visual.svg"
 		}
 	]
--- src/Components/Score/Score.jsx
+++ src/Components/Score/Score.jsx
@@ -1,8 +1,12 @@
 function Score({score,rank, percentile}){
 	return (
 		<div className="bg-gradient-to-r from-indigo-800 via-indigo-600 to-indigo-800 text-white shadow-2xl rounded-2xl flex flex-col items-center p-8 w-60 mr-2">
-			<h2 className="font-bold text-slate-300">Your result</h2>
-			<h2 className="bg-gradient-to-r from-indigo-900 via-indigo-800 to-indigo-900 text-white rounded-full h-20 w-20 text-center p-4 my-4">{score} out of 100</h2>
+      <h2 className="font-bold text-slate-300">Your result</h2>
+      
+      <div className="bg-gradient-to-r from-indigo-900 via-indigo-800 to-indigo-900 text-white rounded-full h-25 w-25 text-center p-7 my-4">
+        <div className="text-4xl font-bold">{score}</div>
+        <p className="text-slate-300/60">out of 100</p>
+      </div>
       <h2>{rank}</h2>
       <p className="text-slate-300/60">You scored higher than {percentile} of the people who have taken this test.</p>
 		</div>)
