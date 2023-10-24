cb40093ec5685bb792559869ebd4ca16abf68f7a
davidjverghese <davidv@summitappliance.com>
2023-10-24 14:22:22 -0400

    work on spacing

--- src/Components/Summary/Summary.jsx
+++ src/Components/Summary/Summary.jsx
@@ -6,9 +6,14 @@ return (
 			<h2 className="font-extrabold text-slate-700 text-xl my-2">Summary</h2>
 			<div className="my-4">
 				{ categories.map((category, index)=>(
-					<div className={`${category.backgroundColor} p-2 my-2 rounded-lg`} key={index}>
-						<img className="inline" src={category.icon} />
-						<span className={ category.textColor }>{ category.category }</span>: <span className="font-bold text-slate-600">{ category.score }</span><span className="text-slate-600"> / 100</span>
+					<div className={`flex justify-between ${category.backgroundColor} p-2 my-3 rounded-lg`} key={index}>
+						<div>
+							<img className="inline pr-2" src={category.icon} />
+							<span className={ category.textColor }>{ category.category }</span>:
+						</div>
+						<div>
+							<span className="font-bold text-slate-600">{ category.score }</span><span className="text-slate-600"> / 100</span>
+						</div>
 					</div>
 				))}
 			</div>
