9bbe6df1229f318bda7dae09e32f9d356d8b2bc8
davidjverghese <davidv@summitappliance.com>
2024-04-12 15:48:43 -0400

    code cleanup

--- src/Components/Score/Score.jsx
+++ src/Components/Score/Score.jsx
@@ -1,17 +1,16 @@
-function Score({score,rank, percentile}){
-	return (
-		<div className="w-full sm:max-w-sm">
-			<div className="bg-background-gradient text-white shadow-2xl rounded-b-2xl sm:rounded-t-2xl flex flex-col items-center p-8">
-				<h2 className="font-bold text-slate-300 text-xl my-2">Your result</h2>
-				<div className="bg-circle-gradient text-white rounded-full h-25 w-25 text-center p-7 my-4">
-					<div className="text-4xl font-bold"><h1>{ score }</h1></div>
-					<p className="text-slate-300/60">out of 100</p>
-				</div>
-				<h2 className="text-xl my-2">{ rank }</h2>
-				<p className="text-slate-200/80 my-2">You scored higher than { percentile } of the people who have taken this test.</p>
-			</div>
-		</div>)
+function Score({score, rank, percentile}){
+  return (
+    <div className="w-full sm:max-w-sm">
+      <div className="bg-background-gradient text-white shadow-2xl rounded-b-2xl sm:rounded-t-2xl flex flex-col items-center p-8">
+        <h2 className="font-bold text-slate-300 text-xl my-2">Your result</h2>
+        <div className="bg-circle-gradient text-white rounded-full h-25 w-25 text-center p-7 my-4">
+          <div className="text-4xl font-bold"><h1>{ score }</h1></div>
+          <p className="text-slate-300/60">out of 100</p>
+        </div>
+        <h2 className="text-xl my-2">{ rank }</h2>
+        <p className="text-slate-200/80 my-2">You scored higher than { percentile } of the people who have taken this test.</p>
+      </div>
+    </div>)
 }
 
 export default Score;
-
--- src/Components/Summary/Summary.jsx
+++ src/Components/Summary/Summary.jsx
@@ -1,26 +1,25 @@
-
 function Summary({categories}){
-return (
-	<div className="sm:pl-5 pl-3 pr-3 w-full sm:max-w-sm">
-		<div className="flex flex-col sm:pt-8">
-			<h2 className="font-extrabold text-slate-700 text-xl my-2">Summary</h2>
-			<div className="my-4">
-				{ categories.map((category, index)=>(
-					<div className={`flex justify-between ${category.backgroundColor} p-2 my-3 rounded-lg text-base`} key={index}>
-						<div>
-							<img alt="" width="30" height="30" className="inline pr-2" src={category.icon} />
-							<span className={ `${category.textColor} font-bold` }>{ category.category }</span>
-						</div>
-						<div>
-							<span className="font-bold text-slate-600">{ category.score }</span><span className="text-slate-600"> / 100</span>
-						</div>
-					</div>
-				))}
-			</div>
-			<button className="bg-dark-gray-blue hover:bg-background-gradient text-white rounded-full items-center p-2 my-2">Continue</button>
-		</div>
-	</div>
-)
+  return (
+    <div className="sm:pl-5 pl-3 pr-3 w-full sm:max-w-sm">
+      <div className="flex flex-col sm:pt-8">
+        <h2 className="font-extrabold text-slate-700 text-xl my-2">Summary</h2>
+        <div className="my-4">
+          { categories.map((category, index)=>(
+            <div className={ `flex justify-between ${ category.backgroundColor } p-2 my-3 rounded-lg text-base` } key={ index }>
+              <div>
+                <img alt="" width="30" height="30" className="inline pr-2" src={ category.icon } />
+                <span className={ `${ category.textColor } font-bold` }>{  category.category }</span>
+              </div>
+              <div>
+                <span className="font-bold text-slate-600">{ category.score }</span><span className="text-slate-600"> / 100</span>
+              </div>
+            </div>
+          )) }
+        </div>
+        <button className="bg-dark-gray-blue hover:bg-background-gradient text-white rounded-full items-center p-2 my-2">Continue</button>
+      </div>
+    </div>
+  )
 }
 
 export default Summary;
