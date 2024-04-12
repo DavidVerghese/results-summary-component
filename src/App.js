4dc3435d907e86a71e1eb243660f1eb0bd3998a9
davidjverghese <davidv@summitappliance.com>
2024-04-12 15:43:14 -0400

    remove unused css rule and clean up code

--- src/App.js
+++ src/App.js
@@ -1,58 +1,47 @@
 import './App.css';
 import Score from './Components/Score/Score';
 import Summary from './Components/Summary/Summary';
-import { useState, useEffect } from 'react';
 
 function App() {
+  const categories = [
+    {
+      "category": "Reaction",
+      "score": 80,
+      "backgroundColor" : "bg-red-300/20",
+      "textColor": "text-light-red",
+      "icon": "assets/images/icon-reaction.svg"
+    },
+    {
+      "category": "Memory",
+      "score": 92,
+      "backgroundColor" : "bg-yellow-200/20",
+      "textColor": "text-orangey-yellow",
+      "icon": "assets/images/icon-memory.svg"
+    },
+    {
+      "category": "Verbal",
+      "score": 61,
+      "backgroundColor" : "bg-green-200/25",
+      "textColor": "text-green-teal",
+      "icon": "assets/images/icon-verbal.svg"
+    },
+    {
+      "category": "Visual",
+      "score": 72,
+      "backgroundColor" : "bg-pale-blue",
+      "textColor": "text-cobalt-blue",
+      "icon": "assets/images/icon-visual.svg"
+    }
+  ]
 
-	// const [text, setText] = useState('');
-
-	// useEffect(() => {
-	// 	fetch('/public/assets/fonts/HankenGrotesk-Medium.ttf')
-	// 		.then(response =>  response.text())
-	// 		.then(data => setText(data));
-	// 	},
-	// []);
-
-	const categories = [
-		{
-			"category": "Reaction",
-			"score": 80,
-			"backgroundColor" : "bg-red-300/20",
-			"textColor": "text-light-red",
-			"icon": "assets/images/icon-reaction.svg"
-		},
-		{
-			"category": "Memory",
-			"score": 92,
-			"backgroundColor" : "bg-yellow-200/20",
-			"textColor": "text-orangey-yellow",
-			"icon": "assets/images/icon-memory.svg"
-		},
-		{
-			"category": "Verbal",
-			"score": 61,
-			"backgroundColor" : "bg-green-200/25",
-			"textColor": "text-green-teal",
-			"icon": "assets/images/icon-verbal.svg"
-		},
-		{
-			"category": "Visual",
-			"score": 72,
-			"backgroundColor" : "bg-pale-blue",
-			"textColor": "text-cobalt-blue",
-			"icon": "assets/images/icon-visual.svg"
-		}
-	]
-
-	return (
-		<div className="flex justify-center sm:mt-5">
-			<div className="bg-white flex flex-col sm:flex-row sm:rounded-2xl shadow-2xl max-sm:w-full">
-				<Score score={76} rank={"Great"} percentile={65} />
-				<Summary categories={categories} />
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex justify-center sm:mt-5">
+      <div className="bg-white flex flex-col sm:flex-row sm:rounded-2xl shadow-2xl max-sm:w-full">
+        <Score score={ 76 } rank={ "Great" } percentile={ 65 } />
+        <Summary categories={ categories } />
+      </div>
+    </div>
+  );
 }
 
 export default App;
